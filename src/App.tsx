1882294f5576c7659c1ac9ddef84b57d373e2917
Alvarotloc <alvarotloc@gmail.com>
2022-04-18 10:51:55 +0200

    Inicio de proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled"
 
-function App() {
+const Heading = styled.h1`
+font-family: 'Lato', sans-serif;
+color: #FFF;
+`
+
+
+const App = ():JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Heading>App</Heading>
+  )
 }
 
-export default App;
+export default App
