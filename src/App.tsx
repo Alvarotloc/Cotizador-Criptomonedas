c9bc7720d6423da0bde7eafca43589729217d1c1
Alvarotloc <alvarotloc@gmail.com>
2022-04-18 11:09:05 +0200

    Hechos los primeros styled components del heading, la imagen y el contenedor, además, perfectamente responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
-import styled from "@emotion/styled"
+import styled from "@emotion/styled";
+import ImagenCripto from "./img/imagen-criptos.png";
+
+const Contenedor = styled.div`
+  max-width: 90rem;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 40rem;
+  width: 80%;
+  margin: 10rem auto 0 auto;
+  display: block;
+`;
 
 const Heading = styled.h1`
-font-family: 'Lato', sans-serif;
-color: #FFF;
-`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+  text-align:center;
+  font-weight:700;
+  margin-top:8rem;
+  margin-bottom:5rem;
+  font-size:3.4rem;
 
+  &::after{
+    content:'';
+    width:10rem;
+    height:.6rem;
+    background-color: #66A2FE;
+    display:block;
+    margin:1rem auto 0 auto;
+  }
+`;
 
-const App = ():JSX.Element => {
+const App = (): JSX.Element => {
   return (
-    <Heading>App</Heading>
-  )
-}
+    <Contenedor>
+      <Imagen src={ImagenCripto} alt="Imagen de criptomonedas" />
+      <div>
+        <Heading>Cotiza Criptomonedas Al Instante</Heading>
+      </div>
+    </Contenedor>
+  );
+};
 
-export default App
+export default App;
